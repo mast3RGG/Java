f1c4b3b334fc4c7e3eca037c2db712879aa9ce83
mast3RGG <morgocialin@gmail.com>
2024-05-20 16:55:06 +0300

    Lesson33

--- Learn_Coding/StaticKeyword/src/Main.java
+++ Learn_Coding/StaticKeyword/src/Main.java
@@ -1,13 +1,31 @@
+
+
 class Demo{
     int x , b;
 
     {
      x = 10;
      b = 10;
+        System.out.println(x + " " + b);
+    }
+    {
+        System.out.println("Second block");
+    }
+
+    static int a ,c ;
+    static {
+        System.out.println("alin");
+        a =15;
+        c = 20;
+        afisare();
+    }
+    static void afisare(){
+        System.out.println(a + " "+c);
     }
 }
 public class Main {
     public static void main(String[] args) {
 
+        Demo d1 = new Demo();
       }
     }
